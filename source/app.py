31af198dbdfda530dc7588c7b44eb86b79fd136e
Rithuraj Nambiar <69073735+rithurajnambiar17@users.noreply.github.com>
2022-11-05 14:22:28 +0530

    added about.html

--- source/app.py
+++ source/app.py
@@ -7,4 +7,8 @@ app.static_folder = 'static'
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 app.run(debug=True)
